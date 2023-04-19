78247ef85e1e22be6c48e2a0760627b14a9fe230
june-s13 <91217925+june-s13@users.noreply.github.com>
2023-04-18 22:13:36 -0500

    script updated to fix map error

--- js/script.js
+++ js/script.js
@@ -3,14 +3,16 @@ const iit = {lat:41.835, lng:-87.627};
 const bowlingSpot = {lat: 41.84890218281394, lng: -87.62504520866703};
 const thefishies = { lat: 30.54719454933027, lng: -174.29905143434593}
 
-async function initMap() {
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: iit,
+      zoom: 14,
+    });
 
-  map = new Map(document.getElementById("map"), {
-    center: iit,
-    zoom: 15,
-  });
+    const marker = new google.maps.Marker({
+      position: bowlingSpot,
+      map: map,
+      title: "Best Bowling Spot on Campus",
+    });
+
 }
-
-initMap();
