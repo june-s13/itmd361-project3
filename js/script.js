32794eb8b08428c32039aa44e3a523f8912f7ffc
june-s13 <91217925+june-s13@users.noreply.github.com>
2023-04-18 21:51:14 -0500

    js script now should load map

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@ const iit = {lat:41.835, lng:-87.627};
 const bowlingSpot = {lat: 41.84890218281394, lng: -87.62504520866703};
 const thefishies = { lat: 30.54719454933027, lng: -174.29905143434593}
 
-function initMap() {
-	map = new google.maps.Map(document.getElementById("map"), {
-		zoom: 15, center:iit,
-	});
-	const marker = new google.maps.Marker({
-		position: iit, map:map,
-	});
+async function initMap() {
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: iit,
+    zoom: 15,
+  });
+}
+
+initMap();
