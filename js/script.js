1370b293ef1e7ac11da7bd4efe5cae40e91ba394
june-s13 <91217925+june-s13@users.noreply.github.com>
2023-04-18 22:26:48 -0500

    There are two markers now

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var map;
-const iit = {lat:41.835, lng:-87.627};
+const iit = {lat:41.834920626248426, lng:-87.62700916218517};
 const bowlingSpot = {lat: 41.84890218281394, lng: -87.62504520866703};
 const thefishies = { lat: 30.54719454933027, lng: -174.29905143434593}
 
@@ -12,7 +12,13 @@ function initMap() {
     const marker = new google.maps.Marker({
       position: bowlingSpot,
       map: map,
-      title: "Best Bowling Spot on Campus",
+      title: "Best bowling spot on campus",
+    });
+
+    const marker2 = new google.maps.Marker({
+      position: thefishies,
+      map: map,
+      title: "My favorite coral reef spot",
     });
 
 }
